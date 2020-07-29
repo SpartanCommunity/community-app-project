794dc10e4c7056abf094f9fe7fcf78b016275463
Dane Murphy <danem7@gmail.com>
2020-07-29 10:29:42 -0700

    below 810px width shows mobile view with drop down menu btn

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -3,16 +3,65 @@ import { FaHandsHelping } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  navLinks = () => {
+    const navLinks = document.querySelectorAll('.navbar-links li');
+    const navbarBtn = document.querySelector('.navbar-btn');
+
+    navbarBtn.addEventListener('click', function () {
+      navLinks.forEach((item, index) => {
+        item.style.animation = `navLinks 0.5s ease forwards ${index / 7 + 6}s`;
+        console.log('test');
+      });
+    });
+  };
+
+  handleToggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
   render() {
     return (
       <nav className="navbar">
         <div className="navbar-center">
-          <div className="navbar-header">
-            <div className="empty-space"></div>
+          <div
+            className={
+              this.state.isOpen ? 'transition-div nav-active' : 'transition-div'
+            }
+          ></div>
+          <div
+            className={
+              this.state.isOpen
+                ? 'navbar-header nav-header-active'
+                : 'navbar-header'
+            }
+          >
             <h1>Community Project</h1>
-            <Link to="/">
+
+            <ul
+              className={
+                this.state.isOpen ? 'navbar-links nav-active' : 'navbar-links'
+              }
+            >
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/my-orgs">My Orgs </Link>
+              </li>
+            </ul>
+            <button
+              type="button"
+              onClick={this.handleToggle}
+              // onClick={this.navLinks}
+              className="navbar-btn"
+            >
               <FaHandsHelping size={35} className="nav-icon" />
-            </Link>
+            </button>
           </div>
         </div>
       </nav>
